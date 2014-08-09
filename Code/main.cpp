11e16d1ae913dfb5b0e529c973e608f5b59c24ed
Zhi Xing <zxing01@syr.edu>
2014-08-08 22:17:02 -0400

    Initial Commit

---
+++ Code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Code
+//
+//  Created by Zhi Xing on 8/8/14.
+//  Copyright (c) 2014 Zhi Xing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
